a01c2e3d6c5f4f034b4bbb0c6382e0e9fc71095b
evgeni <evgeni.lilov88sliven@gmail.com>
2022-10-05 11:40:52 +0200

    get todoList with id

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from asyncio.windows_events import NULL
-from app import app, dtos, models
+from app import app, dtos, models, mongodb
 from flask import request, json
+from bson import ObjectId
 
 @app.route('/')
 def index():
@@ -27,3 +28,22 @@ def getTodoLists():
     for todoList in models.TodoList.objects.all():
       db_model.append(models.TodoList.parse_json(todoList))
     return db_model
+  
+# ?not working route with params
+# @app.route('/getTodoList/<id>', methods = ['POST','GET'])
+@app.route('/getTodoList', methods = ['GET'])
+def getTodoList():
+    id = json.loads(request.data)['id']
+    objInstance = ObjectId(id)   
+    
+    # todoList_json = ''
+    # problem with finding an object with id through the pymodm framework
+    # for todoList in models.TodoList.objects.raw({'legacy_id': objInstance}):
+    # for todoList in models.TodoList.objects.values().get({'_id': id}):
+    #   todoList_json = models.TodoList.parse_json(todoList)
+    
+    # alternative: use MongoClient
+    collection = mongodb.get_db_todo().todo_list
+    res = collection.find_one({"_id": objInstance})
+    
+    return models.TodoList.parse_json(res)
