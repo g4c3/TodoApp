26da3c11e7495928a1fb30f06c45c8961d7aa98e
evgeni <evgeni.lilov88sliven@gmail.com>
2022-10-20 14:49:53 +0200

    yaml definitions

--- app/routes.py
+++ app/routes.py
@@ -7,34 +7,6 @@ from flasgger.utils import swag_from
 
 @app.route('/')
 def index():
-    """Example endpoint returning a list of colors by palette
-    This is using docstrings for specifications.
-    ---
-    parameters:
-      - name: palette
-        in: path
-        type: string
-        enum: ['all', 'rgb', 'cmyk']
-        required: true
-        default: all
-    definitions:
-      Palette:
-        type: object
-        properties:
-          palette_name:
-            type: array
-            items:
-              $ref: '#/definitions/Color'
-      Color:
-        type: string
-    responses:
-      200:
-        description: A list of colors (may be filtered by palette)
-        schema:
-          $ref: '#/definitions/Palette'
-        examples:
-          rgb: ['red', 'green', 'blue']
-    """
     response = '<h1>Todo API! Your IP Address {ip}</h1>'.format(ip = request.remote_addr)
     return response
 
@@ -49,6 +21,7 @@ def createTodoList():
 
 
 @app.route('/getAllTodoLists', methods=['GET'])
+@swag_from('api_doc/TodoList.yml')
 @track_time_spent
 def getTodoLists():
     return TodoList.objects.all().to_json()
